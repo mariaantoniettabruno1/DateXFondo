160d6f08c3c8d66922e2d39bccd13f99d2d5c698
mariaantoniettabruno <maria.bruno750@edu.unito.it>
2022-03-21 18:05:41 +0100

    Inserimento di una tabella in cui vengono visualizzati i dati dell'anno corrente. Posbilità di modificare nota e valore. TODO: inserimento dei bottoni di Duplicazione tabella e blocco modifica

--- wp-content/plugins/dateXFondoPlugin/table/ShortCodeCustomTable.php
+++ wp-content/plugins/dateXFondoPlugin/table/ShortCodeCustomTable.php
@@ -12,124 +12,91 @@ class ShortCodeCustomTable
 
         <html lang="en">
 
-        <head>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-            <script type="text/javascript" src="https://unpkg.com/jquery-tabledit@1.0.0/jquery.tabledit.js"></script>
-            <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-            <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script type="text/javascript" src="https://unpkg.com/jquery-tabledit@1.0.0/jquery.tabledit.js"></script>
+        <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
 
-        </head>
+    </head>
+    <body>
 
-        <body>
+    <h2>TABELLA</h2>
 
+    <table id="data_table" class="table table-striped">
+        <thead>
+        <tr>
+            <th>ID</th>
 
-        <div>
+            <th>Fondo</th>
 
-            <form method='POST' action=''>
+            <th>Ente</th>
 
-                <h4>Seleziona Anno</h4>
+            <th>Anno</th>
 
-                <?php
+            <th>ID Campo</th>
 
-                $years = new CustomTable();
-                $results_years = $years->getAllYears();
-                arsort($results_years);
+            <th>Label Campo</th>
 
-                ?>
+            <th>Descrizione Campo</th>
 
-                <select id='year' name='select_year' onchange='this.form.submit()'>
-                    <option disabled selected> Seleziona Anno</option>
+            <th>Sottotitolo Campo</th>
 
-                    <?php foreach ($results_years as $res_year): ?>
+            <th>Valore</th>
 
-                        <option <?= isset($_POST['select_year']) && $_POST['select_year'] === $res_year[0] ? 'selected' : '' ?>
+            <th>Valore Anno Precedente</th>
 
-                                value='<?= $res_year[0] ?>'><?= $res_year[0] ?></option>
+            <th>Nota</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $year = date("Y");
 
-                    <?php endforeach; ?>
-                </select>
-            </form>
+        $years = new CustomTable();
 
+        $entries = $years->getAllEntries($year);
 
-        </div>
+        foreach ($entries as $entry) {
+            ?>
+            <tr>
+                <td><?php echo $entry [0]; ?></td>
+                <td><?php echo $entry [1]; ?></td>
+                <td><?php echo $entry [2]; ?></td>
+                <td><?php echo $entry [3]; ?></td>
+                <td><?php echo $entry [4]; ?></td>
+                <td><?php echo $entry [5]; ?></td>
+                <td><?php echo $entry [6]; ?></td>
+                <td><?php echo $entry [7]; ?></td>
+                <td><?php echo $entry [8]; ?></td>
+                <td><?php echo $entry [9]; ?></td>
+                <td><?php echo $entry [10]; ?></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+    </body>
 
-        <h2>TABELLA</h2>
+    <script>
 
-            <table id="data_table" class="table table-striped">
-                <thead>
-                <tr>
-                    <th>ID</th>
+        $(document).ready(function () {
 
-                    <th>Fondo</th>
+            $('#data_table').Tabledit({
 
-                    <th>Ente</th>
+                deleteButton: false,
+                editButton: false,
+                columns: {
+                    identifier: [0, 'id'],
+                    editable: [[8, 'valore'], [10, 'nota']]
+                },
+                hideIdentifier: true,
+                url: 'https://demo.mg3.srl/date/wp-json/datexfondoplugin/v1/table/edit'
 
-                    <th>Anno</th>
-
-                    <th>ID Campo</th>
-
-                    <th>Label Campo</th>
-
-                    <th>Descrizione Campo</th>
-
-                    <th>Sottotitolo Campo</th>
-
-                    <th>Valore</th>
-
-                    <th>Valore Anno Precedente</th>
-
-                    <th>Nota</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-        if (isset($_POST['select_year'])) {
-                $years = new CustomTable();
-                $selected_year = $_POST['select_year'];
-
-                $entries = $years->getAllEntries($selected_year);
-
-            foreach ($entries as $entry) {
-                    ?>
-                    <tr>
-                        <td><?php echo $entry [0]; ?></td>
-                        <td><?php echo $entry [1]; ?></td>
-                        <td><?php echo $entry [2]; ?></td>
-                        <td><?php echo $entry [3]; ?></td>
-                        <td><?php echo $entry [4]; ?></td>
-                        <td><?php echo $entry [5]; ?></td>
-                        <td><?php echo $entry [6]; ?></td>
-                        <td><?php echo $entry [7]; ?></td>
-                        <td><?php echo $entry [8]; ?></td>
-                        <td><?php echo $entry [9]; ?></td>
-                        <td><?php echo $entry [10]; ?></td>
-                    </tr>
-                <?php } }?>
-                </tbody>
-            </table>
-            </body>
-
-            <script>
-
-                $(document).ready(function(){
-
-                    $('#data_table').Tabledit({
-
-                        deleteButton: false,
-                        editButton: false,
-                        columns: {
-                            identifier : [0, 'id'],
-                            editable:[[1, 'fondo'], [2, 'ente'], [3, 'anno'],[4, 'id_campo'],[5, 'label_campo'],
-                                [6, 'descrizione_campo'],[7, 'sottotitolo_campo'],[8, 'valore'],[9, 'valore_anno_precedente'],[10, 'nota']]
-                        },
-                        hideIdentifier: true,
-                        url: 'https://demo.mg3.srl/date/wp-json/datexfondoplugin/v1/table/edit'
-
-                    });
-                });
+            });
+        });
 
 
-            </script>
+
         <?php
 
 
--- wp-content/plugins/dateXFondoPlugin/table/live_edit.php
+++ wp-content/plugins/dateXFondoPlugin/table/live_edit.php
@@ -14,62 +14,13 @@ function modifica_campi($request)
 
 
     if (isset($input['action']) && $input['action'] == 'edit') {
-        if (isset($input['fondo'])) {
-            $sql = "UPDATE DATE_entry_chivasso SET fondo=?  WHERE id=?";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("si", $input['fondo'], $input['id']);
-            $res = $stmt->execute();
-            $mysqli->close();
-
-        } else if (isset($input['ente'])) {
-            $sql = "UPDATE DATE_entry_chivasso SET ente=?  WHERE id=?";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("si", $input['ente'], $input['id']);
-            $res = $stmt->execute();
-            $mysqli->close();
-        } else if (isset($input['anno'])) {
-            $sql = "UPDATE DATE_entry_chivasso SET anno=?  WHERE id=?";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("ii", $input['anno'], $input['id']);
-            $res = $stmt->execute();
-            $mysqli->close();
-        } else if (isset($input['id_campo'])) {
-            $sql = "UPDATE DATE_entry_chivasso SET id_campo=?  WHERE id=?";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("ii", $input['id_campo'], $input['id']);
-            $res = $stmt->execute();
-            $mysqli->close();
-        } else if (isset($input['label_campo'])) {
-            $sql = "UPDATE DATE_entry_chivasso SET label_campo=?  WHERE id=?";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("si", $input['label_campo'], $input['id']);
-            $res = $stmt->execute();
-            $mysqli->close();
-        } else if (isset($input['descrizione_campo'])) {
-            $sql = "UPDATE DATE_entry_chivasso SET descrizione_campo=?  WHERE id=?";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("si", $input['descrizione_campo'], $input['id']);
-            $res = $stmt->execute();
-            $mysqli->close();
-        } else if (isset($input['sottotitolo_campo'])) {
-            $sql = "UPDATE DATE_entry_chivasso SET sottotitolo_campo=?  WHERE id=?";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("si", $input['sottotitolo_campo'], $input['id']);
-            $res = $stmt->execute();
-            $mysqli->close();
-        } else if (isset($input['valore'])) {
-            $sql = "UPDATE DATE_entry_chivasso SET valore=?  WHERE id=?";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("si", $input['valore'], $input['id']);
-            $res = $stmt->execute();
-            $mysqli->close();
-        } else if (isset($input['valore_anno_precedente'])) {
-            $sql = "UPDATE DATE_entry_chivasso SET valore_anno_precedente=?  WHERE id=?";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("ii", $input['valore_anno_precedente'], $input['id']);
-            $res = $stmt->execute();
-            $mysqli->close();
-        } else if (isset($input['nota'])) {
+     if (isset($input['valore'])) {
+         $sql = "UPDATE DATE_entry_chivasso SET valore=?  WHERE id=?";
+         $stmt = $mysqli->prepare($sql);
+         $stmt->bind_param("si", $input['valore'], $input['id']);
+         $res = $stmt->execute();
+         $mysqli->close();
+     } else if (isset($input['nota'])) {
             $sql = "UPDATE DATE_entry_chivasso SET nota=?  WHERE id=?";
             $stmt = $mysqli->prepare($sql);
             $stmt->bind_param("si", $input['nota'], $input['id']);
