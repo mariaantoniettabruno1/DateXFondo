49decf3361ebe7c4d28ad5f945743c45bdfb73bf
maria antonietta <mariaantoietta.bruno24@gmail.com>
2023-11-29 15:58:52 +0100

    comment the part of heredity because values aren't anymore

--- wp-content/plugins/dateXFondoPlugin/repositories/ExportDataRepository.php
+++ wp-content/plugins/dateXFondoPlugin/repositories/ExportDataRepository.php
@@ -31,7 +31,7 @@ class ExportDataRepository
 
         $sql = "SELECT * FROM DATE_formula WHERE formula_template_name=?";
         $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param("s",  $request['template_name']);
+        $stmt->bind_param("s", $request['template_name']);
         $res = $stmt->execute();
         $res = $stmt->get_result();
         $formulas = $res->fetch_all(MYSQLI_ASSOC);
@@ -42,28 +42,28 @@ class ExportDataRepository
 
         $sql = "SELECT * FROM DATE_documenti_odt_storico WHERE editor_name=? AND version=?";
         $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param("si",  $request['template_name'], $request['version']);
+        $stmt->bind_param("si", $request['template_name'], $request['version']);
         $res = $stmt->execute();
         $res = $stmt->get_result();
         $odt_documents = $res->fetch_all(MYSQLI_ASSOC);
 
         $sql = "SELECT * FROM DATE_documento_modello_fondo_storico WHERE editor_name=? AND version=?";
         $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param("si",  $request['template_name'], $request['version']);
+        $stmt->bind_param("si", $request['template_name'], $request['version']);
         $res = $stmt->execute();
         $res = $stmt->get_result();
         $modello_fondo_document = $res->fetch_all(MYSQLI_ASSOC);
 
         $sql = "SELECT * FROM DATE_documento_modello_fondo_dati_utili_storico WHERE editor_name=? AND version=?";
         $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param("si",  $request['template_name'], $request['version']);
+        $stmt->bind_param("si", $request['template_name'], $request['version']);
         $res = $stmt->execute();
         $res = $stmt->get_result();
         $modello_dati_utili_document = $res->fetch_all(MYSQLI_ASSOC);
 
         $sql = "SELECT * FROM DATE_documento_modello_fondo_utilizzo_storico WHERE editor_name=? AND version=?";
         $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param("si",  $request['template_name'], $request['version']);
+        $stmt->bind_param("si", $request['template_name'], $request['version']);
         $res = $stmt->execute();
         $res = $stmt->get_result();
         $modello_utilizzo_document = $res->fetch_all(MYSQLI_ASSOC);
@@ -71,61 +71,62 @@ class ExportDataRepository
 
         $sql = "SELECT * FROM DATE_documento_regioni_autonomie_locali_storico WHERE editor_name=? AND version=?";
         $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param("si",  $request['template_name'], $request['version']);
+        $stmt->bind_param("si", $request['template_name'], $request['version']);
         $res = $stmt->execute();
         $res = $stmt->get_result();
         $modello_regioni_document = $res->fetch_all(MYSQLI_ASSOC);
 
         mysqli_close($mysqli);
-        for($i = 0; $i<sizeof($request['cities']);$i++) {
+        for ($i = 0; $i < sizeof($request['cities']); $i++) {
 
             $url = DB_HOST . ":" . DB_PORT . "/";
             $username = DB_USER;
             $password = DB_PASSWORD;
-            $dbname = 'c1date_'.$request['cities'][$i];
+            $dbname = 'c1date_' . $request['cities'][$i];
             $mysqli = new mysqli($url, $username, $password, $dbname);
 
             $anno = $request['anno'] - 1;
-            $sql = "SELECT id_articolo,valore,nota FROM DATE_storico_template_fondo WHERE fondo=? AND anno=? AND template_name=? AND version=?";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("sisi", $request['fondo'], $anno, $request['template_name'], $request['version']);
-            $stmt->execute();
-            $res = $stmt->get_result();
-            $heredity = $res->fetch_all(MYSQLI_ASSOC);
-            if (sizeof($heredity) == 0) {
-                $version = $request['version'] - 1;
-                $sql = "SELECT id_articolo,valore,nota FROM DATE_storico_template_fondo WHERE fondo=? AND anno=? AND template_name=? AND version=?";
-                $stmt = $mysqli->prepare($sql);
-                $stmt->bind_param("sisi", $request['fondo'], $request['anno'], $request['template_name'], $version);
-                $stmt->execute();
-                $res = $stmt->get_result();
-                $heredity = $res->fetch_all(MYSQLI_ASSOC);
-            }
-            for ($i = 0; $i < sizeof($template_data); $i++) {
-                if ($template_data[$i]['heredity'] == '1') {
-                    for ($j = 0; $j < sizeof($heredity); $j++) {
-                        if ($template_data[$i]['id_articolo'] == $heredity[$j]['id_articolo']) {
-                            $template_data[$i]['valore_anno_precedente'] = $heredity[$j]['valore'];
-                        }
-                    }
-                } else if ($template_data[$i]['heredity'] == '2') {
-                    for ($j = 0; $j < sizeof($heredity); $j++) {
-                        if ($template_data[$i]['id_articolo'] == $heredity[$j]['id_articolo']) {
-                            $template_data[$i]['valore_anno_precedente'] = $heredity[$j]['valore'];
-                            $template_data[$i]['nota'] = $heredity[$j]['nota'];
-                        }
-                    }
-                }
-                $template_data[$i]['editable'] = 1;
-            }
+            //questa parte era stata scritta nel caso l'eredità dei dati dovesse partire dal master
+
+            /*            $sql = "SELECT id_articolo,valore,nota FROM DATE_storico_template_fondo WHERE fondo=? AND anno=? AND template_name=? AND version=?";
+                        $stmt = $mysqli->prepare($sql);
+                        $stmt->bind_param("sisi", $request['fondo'], $anno, $request['template_name'], $request['version']);
+                        $stmt->execute();
+                        $res = $stmt->get_result();
+                        $heredity = $res->fetch_all(MYSQLI_ASSOC);
+                        if (sizeof($heredity) == 0) {
+                            $version = $request['version'] - 1;
+                            $sql = "SELECT id_articolo,valore,nota FROM DATE_storico_template_fondo WHERE fondo=? AND anno=? AND template_name=? AND version=?";
+                            $stmt = $mysqli->prepare($sql);
+                            $stmt->bind_param("sisi", $request['fondo'], $request['anno'], $request['template_name'], $version);
+                            $stmt->execute();
+                            $res = $stmt->get_result();
+                            $heredity = $res->fetch_all(MYSQLI_ASSOC);
+                        }*/
+            /*            for ($i = 0; $i < sizeof($template_data); $i++) {
+                            if ($template_data[$i]['heredity'] == '1') {
+                                for ($j = 0; $j < sizeof($heredity); $j++) {
+                                    if ($template_data[$i]['id_articolo'] == $heredity[$j]['id_articolo']) {
+                                        $template_data[$i]['valore_anno_precedente'] = $heredity[$j]['valore'];
+                                    }
+                                }
+                            } else if ($template_data[$i]['heredity'] == '2') {
+                                for ($j = 0; $j < sizeof($heredity); $j++) {
+                                    if ($template_data[$i]['id_articolo'] == $heredity[$j]['id_articolo']) {
+                                        $template_data[$i]['valore_anno_precedente'] = $heredity[$j]['valore'];
+                                        $template_data[$i]['nota'] = $heredity[$j]['nota'];
+                                    }
+                                }
+                            }
+                            $template_data[$i]['editable'] = 1;
+                        }*/
 
             if ($request['version'] != 0) {
                 $version = $request['version'] - 1;
             } else {
                 $version = $request['version'];
             }
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("si", $request['template_name'], $version);
+
 
             $sql = "INSERT INTO DATE_storico_template_fondo 
                     (fondo,anno,descrizione_fondo,ordinamento,id_articolo,sezione,sottosezione,
@@ -133,6 +134,8 @@ class ExportDataRepository
                         SELECT  fondo,anno,descrizione_fondo,ordinamento,id_articolo,sezione,sottosezione,
                      nome_articolo,descrizione_articolo,sottotitolo_articolo,valore,valore_anno_precedente,nota,link,attivo,version,row_type,editable,heredity,template_name 
 FROM DATE_template_fondo WHERE template_name=? AND version=?";
+            $stmt = $mysqli->prepare($sql);
+            $stmt->bind_param("si", $request['template_name'], $version);
             $res = $stmt->execute();
             $sql = "DELETE FROM DATE_template_fondo WHERE template_name=?";
             $stmt = $mysqli->prepare($sql);
@@ -150,7 +153,7 @@ FROM DATE_template_fondo WHERE template_name=? AND version=?";
                     $entry['valore_anno_precedente'], $entry['nota'], $entry['link'], $entry['attivo'], $entry['version'], $entry['row_type'], $entry['heredity'], $entry['template_name']);
                 $res = $stmt->execute();
             }
-            $sql="UPDATE DATE_template_fondo set attivo=0 WHERE row_type='special'";
+            $sql = "UPDATE DATE_template_fondo set attivo=0 WHERE row_type='special'";
             $stmt = $mysqli->prepare($sql);
             $res = $stmt->execute();
 
@@ -167,7 +170,7 @@ FROM DATE_formula WHERE formula_template_name=?";
                      VALUES (?,?,?,?,?,?,?,?,?,?)";
             $stmt = $mysqli->prepare($sql);
             foreach ($formulas as $entry) {
-                $stmt->bind_param("ssssssisii", $entry['sezione'], $entry['sottosezione'], $entry['nome'],  $entry['descrizione'],$entry['condizione'], $entry['formula'],
+                $stmt->bind_param("ssssssisii", $entry['sezione'], $entry['sottosezione'], $entry['nome'], $entry['descrizione'], $entry['condizione'], $entry['formula'],
                     $entry['text_type'], $entry['formula_template_name'], $entry['visibile'], $entry['attivo']);
                 $res = $stmt->execute();
             }
@@ -198,7 +201,7 @@ FROM DATE_documenti_odt WHERE editor_name=? AND version=?";
             $stmt->bind_param("si", $request['template_name'], $version);
             $res = $stmt->execute();
 
-          $sql = "INSERT INTO DATE_documento_modello_fondo_storico
+            $sql = "INSERT INTO DATE_documento_modello_fondo_storico
                     (ordinamento,sezione,sottosezione,nome_articolo,preventivo,document_name,editor_name,anno,attivo,editable,version)
                         SELECT ordinamento,sezione,sottosezione,nome_articolo,preventivo,document_name,editor_name,anno,attivo,editable,version
 FROM DATE_documento_modello_fondo WHERE editor_name=? AND version=?";
@@ -230,16 +233,16 @@ FROM DATE_documento_regioni_autonomie_locali WHERE editor_name=? AND version=?";
             $stmt->bind_param("si", $request['template_name'], $version);
             $res = $stmt->execute();
 
-           $editable = 1;
-           $sql = "INSERT INTO DATE_documenti_odt
+            $editable = 1;
+            $sql = "INSERT INTO DATE_documenti_odt
                    (chiave,valore,document_name,editor_name,anno,active,editable,page,version)
                     VALUES (?,?,?,?,?,?,?,?,?)";
-           $stmt = $mysqli->prepare($sql);
-           foreach ($odt_documents as $entry) {
-               $stmt->bind_param("sissiiisi", $entry['chiave'], $entry['valore'], $entry['document_name'], $entry['editor_name'],
-                   $entry['anno'], $entry['active'], $editable, $entry['page'], $entry['page']);
-               $res = $stmt->execute();
-           }
+            $stmt = $mysqli->prepare($sql);
+            foreach ($odt_documents as $entry) {
+                $stmt->bind_param("sissiiisi", $entry['chiave'], $entry['valore'], $entry['document_name'], $entry['editor_name'],
+                    $entry['anno'], $entry['active'], $editable, $entry['page'], $entry['page']);
+                $res = $stmt->execute();
+            }
 
             $sql = "INSERT INTO DATE_documento_modello_fondo
                     (ordinamento,sezione,sottosezione,nome_articolo,preventivo,document_name,editor_name,anno,attivo,editable,version)
